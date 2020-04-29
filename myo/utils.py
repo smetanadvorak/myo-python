60a55dffc06decc51921ab5ff9ba5bd56c05468a
smetanadvorak <akhmadeus@gmail.com>
2020-04-29 17:14:31 +0200

    replace deprecated time.clock() by time.time()
    time.perf_counter is suggested, but it seems to me that time.time()'s precision should be sufficient for the purpose of this code.

--- myo/utils.py
+++ myo/utils.py
@@ -32,7 +32,7 @@ class TimeInterval(object):
   def __init__(self, value, value_on_reset=None):
     self.value = value
     self.value_on_reset = value_on_reset
-    self.start = time.clock()
+    self.start = time.time()
 
   def check(self):
     """
@@ -41,7 +41,7 @@ class TimeInterval(object):
 
     if self.value is None:
       return True
-    return (time.clock() - self.start) >= self.value
+    return (time.time() - self.start) >= self.value
 
   def reset(self, value=None):
     """
@@ -49,7 +49,7 @@ class TimeInterval(object):
     """
 
     if value is None:
-      value = time.clock()
+      value = time.time()
     self.start = value
     if self.value_on_reset:
       self.value = self.value_on_reset
@@ -74,7 +74,7 @@ class TimeoutManager(TimeInterval):
 
     if self.value is None:
       return False
-    return (time.clock() - self.start) >= self.value
+    return (time.time() - self.start) >= self.value
 
   def remainder(self, max_value=None):
     """
@@ -84,7 +84,7 @@ class TimeoutManager(TimeInterval):
 
     if self.value is None:
       return max_value
-    remainder = self.value - (time.clock() - self.start)
+    remainder = self.value - (time.time() - self.start)
     if remainder < 0.0:
       return 0.0
     elif max_value is not None and remainder > max_value:
